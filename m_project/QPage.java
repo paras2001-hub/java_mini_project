2d8e8d47db7e7aeeac261c2666666c7d85b38bfd
paras.p2001 <72026773+paras2001-hub@users.noreply.github.com>
2020-11-21 16:10:59 +0530

    More changes required

--- m_project/QPage.java
+++ m_project/QPage.java
@@ -3,77 +3,73 @@
 class Qpage {
     public static void main(String args[]){
 
-        Page_Layout p1 = new Page_Layout(1,"1/2 + 5/7 =","14/17","15/17","13/17","12/17","none");
+        JFrame pg = new JFrame();
+        pg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pg.setSize(600, 400);
+        pg.setResizable(false);
+        pg.setTitle("Fraction Quiz");
+        pg.setVisible(true);
+        pg.setLayout(new GridLayout(
+             4,
+             1,
+             30,
+             0
+        ));
 
+        JLabel num = new JLabel();
+        num.setFont(new Font("Helvetica Neue",Font.BOLD,20));
+        JPanel questionNo = new JPanel();
+        questionNo.setBackground(Color.WHITE);
+        questionNo.setBounds(0, 0, 600, 100);
+        
+        JLabel q = new JLabel();
+        q.setFont(new Font("Helvetica Neue",Font.PLAIN,20));
+        JPanel question = new JPanel(); 
+        question.setBackground(Color.WHITE);
 
+        JPanel options = new JPanel();
+        options.setLayout(new FlowLayout(
+              FlowLayout.CENTER,
+              60,
+              40
+        ));
+        options.setBackground(Color.DARK_GRAY);
+        JButton op1 = new JButton();
+        op1.setFocusable(false);
+        JButton op2 = new JButton();
+        op2.setFocusable(false);
+        JButton op3 = new JButton();
+        op3.setFocusable(false);
+        JButton op4 = new JButton();
+        op4.setFocusable(false);
+        
+        JLabel timerem = new JLabel("TIME REMAINING : NULL");
+        timerem.setFont(new Font("Helvetica Neue",Font.BOLD,17));
+        timerem.setForeground(Color.WHITE);
+        JPanel timePanel = new JPanel();
+        timePanel.add(timerem);
+        timePanel.setBackground(Color.DARK_GRAY);
 
-        // JFrame pg = new JFrame();
-        // pg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // pg.setSize(600, 400);
-        // pg.setResizable(false);
-        // pg.setTitle("Fraction Quiz");
-        // pg.setLayout(new GridLayout(
-        //     4,
-        //     1,
-        //     30,
-        //     0
-        // ));
+        for(int i = 0, i<10, i++){
+            num.setText("Question No. 1 :");
+            q.setText(" 1/2 + 5/7 = ");
+            question.add(q);
+            questionNo.add(num);
 
-        // JLabel num = new JLabel();
-        // num.setText("Question No. 1 :");
-        // num.setFont(new Font("Helvetica Neue",Font.BOLD,20));
-        // JPanel questionNo = new JPanel();
-        // questionNo.add(num);
-        // questionNo.setBackground(Color.WHITE);
-        // questionNo.setBounds(0, 0, 600, 100);
+            op1.setText("17/14");
+            op2.setText("13/14");
+            op3.setText("15/14");
+            op4.setText("19/14");
 
-        // JLabel q = new JLabel(" 1/2 + 5/7 = ");
-        // q.setFont(new Font("Helvetica Neue",Font.PLAIN,20));
-        // JPanel question = new JPanel();
-        // question.add(q);
-        // question.setBackground(Color.WHITE);
+            options.add(op1);
+            options.add(op2);
+            options.add(op3);
+            options.add(op4);
 
-
-        // JPanel options = new JPanel();
-        // options.setLayout(new FlowLayout(
-        //     FlowLayout.CENTER,
-        //     60,
-        //     40
-        // ));
-        // options.setBackground(Color.DARK_GRAY);
-        // JButton op1 = new JButton();
-        // op1.setText("17/14");
-        // op1.setFocusable(false);
-        // JButton op2 = new JButton();
-        // op2.setFocusable(false);
-        // op2.setText("13/14");
-        // JButton op3 = new JButton();
-        // op3.setText("15/14");
-        // op3.setFocusable(false);
-        // JButton op4 = new JButton();
-        // op4.setText("19/14");
-        // op4.setFocusable(false);
-        // options.add(op1);
-        // options.add(op2);
-        // options.add(op3);
-        // options.add(op4);
-
-        // JLabel timerem = new JLabel("TIME REMAINING : 29");
-        // timerem.setFont(new Font("Helvetica Neue",Font.BOLD,17));
-        // timerem.setForeground(Color.WHITE);
-        // JPanel timePanel = new JPanel();
-        // timePanel.add(timerem);
-        // timePanel.setBackground(Color.DARK_GRAY);
-
-
-
-
-        // pg.add(questionNo);
-        // pg.add(question);
-        // pg.add(options);
-        // pg.add(timePanel);
-        // pg.setVisible(true);
-
-
+            pg.add(questionNo);
+            pg.add(question);
+            pg.add(options);
+            pg.add(timePanel);
+        }
     }
 }
