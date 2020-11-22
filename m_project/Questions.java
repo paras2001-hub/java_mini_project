62b41f5d843b9a95b2c508c7b0a14319da5c319f
ikjotkhurana <61426835+ikjotkhurana@users.noreply.github.com>
2020-11-22 14:15:08 +0530

    Add files via upload

--- m_project/Questions.java
+++ m_project/Questions.java
@@ -8,7 +8,47 @@ public class Questions {
 
    public String getQuestion() {
        int a,b,c,d;
-       a = ((Math.random() * 10));
+       int max=20,min=1;
+       a = (int)(Math.random() * (max - min + 1) + min);
+       b = (int)(Math.random() * (max - min + 1) + min);
+       c = (int)(Math.random() * (max - min + 1) + min);
+       d = (int)(Math.random() * (max - min + 1) + min);
+       max = 5;
+       min = 1;
+       int num,deno;
+       int randOperator = (int)(Math.random() * (max - min + 1) + min);
+       
+       switch(randOperator){
+           case 1:
+           num = a*d + c*b;
+           deno = b*d;
+           this.correctAnswer = num +"/"+ deno;
+           this.operator="+";
+           break;
+
+           case 2:
+           num = a*d - c*b;
+           deno = b*d;
+           this.correctAnswer = num +"/"+ deno;
+           this.operator="-";
+           break;
+
+           case 3:
+           num = a*c;
+           deno = b*d;
+           this.correctAnswer = num +"/"+ deno;
+           this.operator="*";
+           break;
+
+           case 4:
+           num = a*d;
+           deno = c*b;
+           this.correctAnswer = num +"/"+ deno;
+           this.operator="/";
+           break;
+       }
    }
+
+
 
 }
