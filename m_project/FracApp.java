df07d738816db3754642ee35ca5dfe4426ed6d1b
paras.p2001 <72026773+paras2001-hub@users.noreply.github.com>
2020-12-02 21:02:48 +0530

    Timer modified

--- m_project/FracApp.java
+++ m_project/FracApp.java
@@ -1,32 +1,44 @@
 import java.util.*;
 
 class FracApp{
+    static int ulC, olC,ulW, olW;
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         int playagain = 1;
-        int ulC, olC, ulW, olW;
         System.out.println("Enter time per question: ");
         int time = s.nextInt();
         System.out.println("\n\n========== RULES ==========\n\n");
 
         while (playagain == 1) {
+
+            ulC = 0;
+            olC = 0;
+            ulW = 0;
+            olW = 0;
+
             for (int i = 1; i <= 10; i++) {
+
                 int timerflag = 0;
                 Qtimer qtimer = new Qtimer();
                 QandA mcq = new QandA();
+                qtimer.runTimer();
+
                 System.out.print("Q.No. " + i + " => ");
                 System.out.println(mcq.getQuestion());
-                qtimer.runTimer();
+                
                 ArrayList<String> options = mcq.getChoices();
                 System.out.println("1)"+options.get(0));
                 System.out.println("2)"+options.get(1));
                 System.out.println("3)"+options.get(2));
                 System.out.println("4)"+options.get(3));
                 int answer = s.nextInt();
+
+
                 if(qtimer.getsec()<=time){
                     timerflag = 1;
                 }
                 int ansflag = checkans(mcq, answer, options);
+
                 qtimer.endtimer();
             }
 
@@ -34,8 +46,7 @@ class FracApp{
             printScore();
             System.out.println("\n============================\n");
             System.out.println("Do you want to play again?(y/n)");
-            
-            while (playagain == 1 || playagain == 0) {
+            while (playagain == 2) {
                 char playAgain = s.next().charAt(0);
                 switch (playAgain) {
                 case 'Y' :
@@ -48,8 +59,7 @@ class FracApp{
                     break;
                 default : 
                     System.out.println("Invalid Option!");
-                    playagain = 2;
-                    
+                    playagain = 2;   
             }
         }
         }
@@ -60,27 +70,27 @@ class FracApp{
         String c_answer = mcq.getCorrectAnswer();
         switch(answer){
             case 1:
-            if(c_answer.equals(option.get(0))){
-                f = 1;
-            }
-            break;
+                if(c_answer.equals(option.get(0))){
+                    f = 1;
+                }
+                break;
             case 2:
-            if(c_answer.equals(option.get(1))){
-                f = 1;
-            }
-            break;
+                if(c_answer.equals(option.get(1))){
+                    f = 1;
+                }
+                break;
             case 3:
-            if(c_answer.equals(option.get(2))){
-                f = 1;
-            }
-            break;
+                if(c_answer.equals(option.get(2))){
+                    f = 1;
+                }
+                break;
             case 4:
-            if(c_answer.equals(option.get(3))){
-                f = 1;
-            }
-            break;
+                if(c_answer.equals(option.get(3))){
+                    f = 1;
+                }
+                break;
             default:
-            System.out.println("Invalid Option!");
+                System.out.println("Invalid Option!");
         }
         return f;
     }
